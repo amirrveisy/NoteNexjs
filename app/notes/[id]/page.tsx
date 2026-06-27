28a949b088849a21e0adaecf6910606be2448897
Amirrveisy <s307522@studenti.polito.it>
2026-06-27 14:14:30 +0200

    database first touch

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,42 +1,42 @@
-import { notFound } from "next/navigation";
-import { getNoteById } from "@/app/services/notes";
-import { toggleNoteImportance } from "@/app/actions/notes";
+// import { notFound } from "next/navigation";
+// import { getNoteById } from "@/app/services/notes";
+// import { toggleNoteImportance } from "@/app/actions/notes";
 
 
-const NotePage = async ({ params }: { params: Promise<{ id: string }> }) => {
+// const NotePage = async ({ params }: { params: Promise<{ id: string }> }) => {
 
-    const { id } = await params
+//     const { id } = await params
 
-    const note = getNoteById(Number(id))
-
-
-    if (!note) {
-        notFound()
-    }
-
-    return (
-
-        <div>
-
-            <h2>{note.content}</h2>
-            <p>{note.important ? "Important" : "Not important"}</p>
-
-            <form action={toggleNoteImportance}>
-
-                <input type="hidden" name="id" value={note.id} />
-                <button>
-
-                    {note.important ? "Mark as not important" : "Mark as important"}
-
-                </button>
-
-            </form>
-
-        </div>
+//     const note = getNoteById(Number(id))
 
 
-    )
+//     if (!note) {
+//         notFound()
+//     }
 
-}
+//     return (
 
-export default NotePage
+//         <div>
+
+//             <h2>{note.content}</h2>
+//             <p>{note.important ? "Important" : "Not important"}</p>
+
+//             <form action={toggleNoteImportance}>
+
+//                 <input type="hidden" name="id" value={note.id} />
+//                 <button>
+
+//                     {note.important ? "Mark as not important" : "Mark as important"}
+
+//                 </button>
+
+//             </form>
+
+//         </div>
+
+
+//     )
+
+// }
+
+// export default NotePage
